24c914fb047be87b6d14d343d65dbeb28e8301d6
Letícia dos Santos <lds.leticia.dos.santos@gmail.com>
2022-03-16 15:53:38 +0000

    arrays e loops exercicios

---
+++ arrays_e_loops/explicacao_arrays_e_loops.js
@@ -0,0 +1,22 @@
+let videoGames = ["PS4", "XBOX", "Nintendo"]
+for (let i = 0; i < videoGames.length; i++){
+    console.log(videoGames[i])
+}
+
+videoGames.forEach(function(item){
+    console.log(item)
+});
+
+frutas = ['banana', 'pera', 'uva', 'abacaxi']
+frutas.forEach(function(fruta, index){
+    console.log(fruta, index)
+})
+// for(let numero = 0; numero <= 10; numero++){
+//     console.log(numero)
+// }
+
+// let i = 0;
+// while (i < 10){
+//     console.log(i);
+//     i++;
+// }
--- arrays_e_loops/script.js
+++ arrays_e_loops/script.js
@@ -1,7 +1,25 @@
-let videoGames = ["ABC", "ABC", "ABC"]
-videoGames.push("DEF")
-videoGames.length
+// Crie um array com os anos em que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+let anosGanhos = [1959, 1962, 1970, 1994, 2002]
 
-for(let numero = 0; numero <= 10; numero++){
-    console.log(numero)
+// Interaja com o array utilizando um loop para mostrar a mensagem
+// "O Brasil ganhou a copa de ${ano}"
+anosGanhos.forEach(function(ano){
+    console.log(`O Brasil ganhou a copa de ${ano}`)
+});
+
+// Inclua mais uma fruta no array
+let frutas = ['banana', 'maçã', 'pêra', 'uva', 'melância']
+frutas.push("Amora")
+console.log(frutas)
+
+// Faça com que o loop pare quando chegar em pera
+for( let i = 0; i < frutas.length; i++){
+    console.log(frutas[i])
+    if (frutas[i] === 'pêra'){
+        break
+    }
 }
+
+ultimaFruta = frutas[frutas.length -1]
+console.log(ultimaFruta)
