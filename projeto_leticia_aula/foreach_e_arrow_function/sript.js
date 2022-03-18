bab5fee7d66c7d53983f1bdc86367a09dfc03407
Letícia dos Santos <lds.leticia.dos.santos@gmail.com>
2022-03-18 19:45:39 +0000

    forEach e ArrowFunction

--- projeto_leticia_aula/foreach_e_arrow_function/sript.js
+++ projeto_leticia_aula/foreach_e_arrow_function/sript.js
@@ -1,13 +1,49 @@
+// const imgs = document.querySelectorAll('img')
+
+// let i = 0;
+// imgs.forEach(function(item, index, array) { 
+//     console.log(item, index, array)
+// });
+
+// const titulos = document.getElementsByClassName('titulo');
+// const titulosArray = Array.from(titulos)
+
+// titulosArray.forEach((item) => {
+//     console.log(item)
+// });
+
+
+
+// Mostre no console cada parágrafo do site
+const paragrafos = document.querySelectorAll('p')
+
+paragrafos.forEach((item) => {
+    console.log(item)
+})
+
+// Mostre o texto dos parágrafos no console
+paragrafos.forEach((item) => {
+    console.log(item.innerText)
+})
+
+// Corrija os comandos abaixo:
+// const imgs = document.querySelectorAll('img')
 const imgs = document.querySelectorAll('img')
 
+// imgs.forEach(item, index => {
+//     console.log(item, index)
+// })
+imgs.forEach((item, index) => {
+    console.log(item, index)
+})
+
+// let i = 0;
+// imgs.forEach( => {
+//     console.log(i++)
+// });
 let i = 0;
-imgs.forEach(function(item, index, array) { 
-    console.log(item, index, array)
+imgs.forEach( () => {
+    console.log(i++)
 });
 
-const titulos = document.getElementsByClassName('titulo');
-const titulosArray = Array.from(titulos)
-
-titulosArray.forEach((item) => {
-    console.log(item)
-});
+imgs.forEach(() => i++)
