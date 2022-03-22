1fd10f56ee67a21ea1344d3d9ea47e29e36e845e
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-03-22 19:30:51 -0300

    dimensoes e distancia

---
+++ projeto_leticia_aula/dimensoes-e-distancia/sript.js
@@ -0,0 +1,7 @@
+const listaAnimais = document.querySelector('.animais-lista')
+
+console.log(listaAnimais.scrollHeight)
+
+const section = document.querySelector('.animais')
+section.getBoundingClientRect()
+console.log(section)
