1c277692101500aa840ec7dff5f25c72cd27c892
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-03-22 20:31:40 -0300

    eventos

---
+++ projeto_leticia_aula/eventos/sript.js
@@ -0,0 +1,40 @@
+// const img = document.querySelector('img')
+
+// function callback(event) {
+//     console.log(this)
+//     console.log(this.getAttribute('src'))
+// }
+
+// img.addEventListener('click', callback)
+
+// const h1 = document.querySelector("h1");
+// function handleEvent(event) {
+//   console.log(event.type, event);
+//   console.log("cliquei");
+// }
+
+// h1.addEventListener('click', handleEvent) //clicou, mostra
+// h1.addEventListener('mouseenter', handleEvent) // passou o mouse, mostra
+// h1.addEventListener('mousemove', handleEvent) // mexeu o mouse, mostra
+
+// window.addEventListener('scroll', handleEvent) // mostra com srow
+// window.addEventListener('resize', handleEvent) // mostra ao dar resize
+// window.addEventListener('keydown', handleEvent) // mostra quando clica no teclado
+
+// function handleKeyboard(event) {
+//     if(event.key === 'a') {
+//         document.body.classList.toggle('azul')
+//     }
+// }
+
+// window.addEventListener('keydown',handleKeyboard)
+
+// function handleImg(event){
+//     console.log(event.target.getAttribute('src'))
+// }
+
+// let imgs = document.querySelectorAll('img')
+// imgs.forEach((img) =>{
+//     img.addEventListener('click', handleImg)
+// })
+
