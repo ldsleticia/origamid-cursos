bdb1fb46326e96bb076bd0e7811f575bf183b96b
Letícia dos Santos <lds.leticia.dos.santos@gmail.com>
2022-03-30 23:56:29 +0000

    array e iteracao

---
+++ array_e_iteracao/script.js
@@ -0,0 +1,12 @@
+// let carros = ['ford', 'fiat', 'honda']
+
+// carros.forEach((item, index, array) => {
+//     carros[index] = 'teste'
+//     console.log(item.toUpperCase, index, array)
+// })
+
+let li = document.querySelectorAll('li')
+
+li.forEach((item) => {
+    item.classList.add('ativa')
+})
