b752462ee1ea617248362acd39f83dc7f3ddc978
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-06-05 21:57:30 -0300

    seleção de elementos e forEach

--- selecao_de_elementos/script.js
+++ selecao_de_elementos/script.js
@@ -1,29 +1,28 @@
 // Retorne no console todas as imagens do site
-const imgs = document.querySelectorAll('img')
-console.log(imgs)
+const imgs = document.querySelectorAll("img");
+console.log(imgs);
 
 // Retorne no console apenas as imagens que começarem com a palavra imagem
-const img = document.querySelectorAll('[src^="./img/imagem"]')
-console.log(img)
+const img = document.querySelectorAll('[src^="./img/imagem"]');
+console.log(img);
 
-// Selecione todos os links internos 
-const linksInternos = document.querySelectorAll('[href^="#"]')
-console.log(linksInternos)
+// Selecione todos os links internos
+const linksInternos = document.querySelectorAll('[href^="#"]');
+console.log(linksInternos);
 
 // Selecione o primeiro h2 dentro de .animais-descrição
-const h2Animais = document.querySelector('.animais-descricao h2')
-console.log(h2Animais.innerText)
+const h2Animais = document.querySelector(".animais-descricao h2");
+console.log(h2Animais.innerText);
 
 // Selecione o último p do site
-const ultimoP = document.querySelector('footer p:last-child')
-console.log(ultimoP.innerText)
-
+const ultimoP = document.querySelector("footer p:last-child");
+console.log(ultimoP.innerText);
 
 // forEach()
-img.forEach(function(item) {
-    console.log(item)
-})
+img.forEach(function (item) {
+  console.log(item);
+});
 
 imgs.forEach((item) => {
-    console.log(item)
-})
+  console.log(item);
+});
