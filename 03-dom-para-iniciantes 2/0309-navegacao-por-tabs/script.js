fe32c9b4a051c4c47183f103fe01ee4a3b05fb65
ldsleticia <lds.leticia.dos.santos@gmail.com>
2024-02-16 09:27:43 -0300

    navegacao por tabs

--- 03-dom-para-iniciantes 2/0309-navegacao-por-tabs/script.js
+++ 03-dom-para-iniciantes 2/0309-navegacao-por-tabs/script.js
@@ -1,18 +1,17 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if(tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add('ativo');
-
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add("ativo");
   function activeTab(index) {
     tabContent.forEach((section) => {
-      section.classList.remove('ativo');
+      section.classList.remove("ativo");
     });
-    tabContent[index].classList.add('ativo');
+    tabContent[index].classList.add("ativo");
   }
 
   tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
+    itemMenu.addEventListener("click", () => {
       activeTab(index);
     });
   });
