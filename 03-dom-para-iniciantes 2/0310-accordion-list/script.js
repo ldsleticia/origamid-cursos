e41fe00630b8dc415f40cb38778e9f8e25562ee3
ldsleticia <lds.leticia.dos.santos@gmail.com>
2024-02-16 10:07:47 -0300

    accordion list

--- 03-dom-para-iniciantes 2/0310-accordion-list/script.js
+++ 03-dom-para-iniciantes 2/0310-accordion-list/script.js
@@ -1,42 +1,38 @@
 function initTabNav() {
-  const tabMenu = document.querySelectorAll('.js-tabmenu li');
-  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-  if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
-
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
     function activeTab(index) {
       tabContent.forEach((section) => {
-        section.classList.remove('ativo');
+        section.classList.remove("ativo");
       });
-      tabContent[index].classList.add('ativo');
+      tabContent[index].classList.add("ativo");
     }
 
     tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
+      itemMenu.addEventListener("click", () => {
         activeTab(index);
       });
     });
   }
 }
-initTabNav();
+
+function activeAccordion() {
+  this.classList.toggle("ativo");
+  this.nextElementSibling.classList.toggle("ativo");
+}
 
 function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-accordion dt');
-  const activeClass = 'ativo';
-  
-  if(accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+  const accordionList = document.querySelectorAll(".js-accordion dt");
 
-    function activeAccordion() {
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
-    }
-
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+  if (accordionList.length) {
+    accordionList.forEach((itemAccordion) => {
+      itemAccordion.addEventListener("click", activeAccordion);
     });
   }
 }
-initAccordion();
+
+initTabNav();
+initAccordion()
