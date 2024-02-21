56cfca7628d29ed6c9bfe40ffe9c120f3758dda4
ldsleticia <lds.leticia.dos.santos@gmail.com>
2024-02-20 22:58:46 -0300

    animacao ao scroll

--- 03-dom-para-iniciantes 2/0312-animacao-ao-scroll/script.js
+++ 03-dom-para-iniciantes 2/0312-animacao-ao-scroll/script.js
@@ -1,91 +1,58 @@
 function initTabNav() {
-  const tabMenu = document.querySelectorAll('.js-tabmenu li');
-  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-  if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
-
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
     function activeTab(index) {
       tabContent.forEach((section) => {
-        section.classList.remove('ativo');
+        section.classList.remove("ativo");
       });
-      tabContent[index].classList.add('ativo');
+      tabContent[index].classList.add("ativo");
     }
 
     tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
+      itemMenu.addEventListener("click", () => {
         activeTab(index);
       });
     });
   }
 }
-initTabNav();
+
+function activeAccordion() {
+  this.classList.toggle("ativo");
+  this.nextElementSibling.classList.toggle("ativo");
+}
 
 function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-accordion dt');
-  const activeClass = 'ativo';
-  
-  if(accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+  const accordionList = document.querySelectorAll(".js-accordion dt");
 
-    function activeAccordion() {
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
-    }
-
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+  if (accordionList.length) {
+    accordionList.forEach((itemAccordion) => {
+      itemAccordion.addEventListener("click", activeAccordion);
     });
   }
 }
-initAccordion();
 
-function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
 
-  function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-
-    // forma alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: 'smooth',
-    // });
-  }
-
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
   });
 }
-initScrollSuave();
 
-function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('.js-scroll');
-  if(sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
+function softScroll() {
+  const internalLinks = document.querySelectorAll(".js-menu a[href^='#']");
 
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
-          section.classList.add('ativo');
-        else 
-          section.classList.remove('ativo');
-      })
-    }
-
-    animaScroll();
-
-    window.addEventListener('scroll', animaScroll);
-  }
+  internalLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
 }
-initAnimacaoScroll();
+
+initTabNav();
+initAccordion();
+softScroll();
